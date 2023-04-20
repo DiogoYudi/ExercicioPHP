3e66ead02b0949ab6414b22f73548f05fc535feb
DiogoYudi <diogoyukui23@gmail.com>
2023-04-20 15:52:41 -0300

    4

---
+++ ex4.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Exercício 4 <br>";
+    echo "Faça um programa em PHP, que dado a distância total (em km) percorrida por um automóvel e a quantidade de combustível (em litros) consumida para percorrê-la, calcule e imprima o consumo médio de combustível, sabendo que: <br> Consumo médio = Distancia percorrida/Litros Consumidos <br>";
+    $dp = 5;
+    $litro = 2;
+    $cm = $dp/$litro;
+    echo "Consumo médio(Km/L) = {$cm}";
+?>
