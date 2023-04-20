d551ed9d13fb50b05b3d18298fb27104b089f431
DiogoYudi <diogoyukui23@gmail.com>
2023-04-20 15:53:45 -0300

    6

---
+++ ex6.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Exercício 6 <br>";
+    echo "Faça um programa em PHP, que dado três números inteiros n1, n2 e n3. Verifique e mostre qual é o maior deles. <br>";
+    $n1 = 1;
+    $n2 = 2;
+    $n3 = 3;
+    $maior = max($n1, $n2, $n3);
+    echo "Maior valor = {$maior}";
+?>
