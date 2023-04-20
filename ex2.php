a9782bb4cedb024c57a26fcdf792a86f6b2ff08f
DiogoYudi <diogoyukui23@gmail.com>
2023-04-20 15:45:57 -0300

    Inserção do ex1

---
+++ ex2.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Exercício 2 <br>";
+    echo "Faça um programa em PHP, que dado um valor em Dólar (US$) e a cotação do Dólar atual. Converta o valor dado em Dólar para Real (R$) e mostre o resultado em Real (R$) para o usuário. Obs: Real = Dolar ∗ CotaçãoDolar <br>";
+    $dolar = 50;
+    $cotacao = 5.5;
+    $real = $dolar * $cotacao;
+    echo "Dolar para Real: <br> <br> Dolar: {$dolar} <br> Real: {$real}";
+?>
